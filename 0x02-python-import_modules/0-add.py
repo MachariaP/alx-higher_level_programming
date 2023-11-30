27edb94359b3d8946e1d8ae5dc1e309ca28915c4
MachariaP <walburphinehas78@gmail.com>
2023-11-30 13:47:51 +0300

    modified my function implementation

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 
-import add_0
+if __name__ == "__main__":
 
-a = 1
-b = 2
+    from add_0 import add
 
-print(f"{a} + {b} = {add_0.add(a, b)}")
+    a = 1
+    b = 2
+
+    print("{} + {} = {}".format(a, b, add(a,b)))
