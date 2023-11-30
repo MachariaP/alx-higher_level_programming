44d5da90a86d9a9d58c950f4139cc9766a900636
MachariaP <walburphinehas78@gmail.com>
2023-11-30 14:06:38 +0300

    modified pycodestyle and added comments

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python3
 
-"""
-This script demonstrates the use of the add function from the add_0 module.
-
-It imports the add function, assigns values to variables a and b, and prints the results of the addation using the add function.
-"""
-
 if __name__ == "__main__":
 
     from add_0 import add
