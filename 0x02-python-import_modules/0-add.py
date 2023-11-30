9f34a0dfa3c7a7f2148606349b4a3723552271bd
MachariaP <walburphinehas78@gmail.com>
2023-11-30 13:55:42 +0300

    modified pycodestyle and added comments

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 
+# If the script is executed as the main program
 if __name__ == "__main__":
-
+    # Import the add function from the add_0 module
     from add_0 import add
 
+    # Assign values to variables a and b
     a = 1
     b = 2
 
+    # Print the result of the addition using the add function
     print("{} + {} = {}".format(a, b, add(a, b)))
