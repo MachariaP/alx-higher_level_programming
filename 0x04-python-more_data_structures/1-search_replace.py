a94165b30716fab772072c2701d4b2dc0e71da5c
MachariaP <walburphinehas78@gmail.com>
2023-12-06 15:43:48 +0300

    modified my file to add comments

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,14 +1,23 @@
 #!/usr/bin/python3
 
 def search_replace(my_list, search, replace):
+     """
+    replaces all occurrences of an element by another in a new list.
+    Args:
+        my_list: initial list
+        search: element to replace in the list
+        replace: new element
+    Returns:
+        new list
+    """
     # create a new list to store the replaced elements
     new_list = []
 
     # Iterate through each element in the original list
-    for element in my_list:
-        if element == search:
+    for i in my_list:
+        if i == search:
             new_list.append(replace)
         else:
-            new_list.append(element)
+            new_list.append(i)
 
     return new_list
