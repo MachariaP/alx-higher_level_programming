e46435ad15339ae475fb21e0f0d5e16bc1cdf76c
MachariaP <walburphinehas78@gmail.com>
2023-12-18 11:28:56 +0300

    wrote a python script function that prints x elements of a list.

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x = 0):
+    """
+        safe_print_list - prints x elements of a list
+        Args:
+            my_list = []: input list
+            x: number of elements to print
+        Returns:
+            real number of elements printed
+    """
     try:
         count = 0
         for i in range(x):
