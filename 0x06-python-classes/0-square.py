e7fa3930c3d7e09091a2297191cf53d460bb2ea1
MachariaP <walburphinehas78@gmail.com>
2023-12-19 16:52:04 +0300

    wrote a python script of an empty class Square that defines a square

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+class Square:
+    """
+    This is the Square class.
+
+    It is currently empty and can be used as a template for defining a square.
+
+    Attributes:
+        None
+
+    Methods:
+        None
+    """
+    pass
