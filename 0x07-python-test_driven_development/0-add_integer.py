d3c63550e72ce0376151fafac397d825ee2665f3
MachariaP <walburphinehas78@gmail.com>
2024-01-03 12:56:51 +0300

    run a python script function that adds two integers.

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -3,43 +3,22 @@
 """
 Module that adds 2 integers.
 
-add_integer, that takes two integer arguments and returns their sum.
+add_integer, takes two integer arguments and return their sum.
 """
 
 
 def add_integer(a, b=98):
     """
-    Compute and return the sum of two integers.
-
+    adds 2 integers.
     Args:
-        a: first integer
-        b: second integer, 98 by default
-
-    Returns:
-        sum of integer a and integer b
-
-    Tests:
-        >>> add_integer(0)
-        98
-        >>> add_integer(1, -2)
-        -1
-        >>> add_integer('3', '23')
-        Traceback (most recent call last):
-        ...
-        TypeError: a must be an integer
+        a: first int
+        b: second int, default 98
+    Returns: sum of a and b
     """
-    if not isinstance(a, (int, float)):
+    if type(a) not in (int, float):
         raise TypeError("a must be an integer")
-
-    if not isinstance(b, (int, float)):
+    if type(b) not in (int, float):
         raise TypeError("b must be an integer")
-
-    try:
-        int(a)
-        int(b)
-    except ValueError as err:
-        raise err
-
     return int(a) + int(b)
 
 
