05e2d911f0e1dff9bcfa55a17f8fc73df3c68d76
MachariaP <walburphinehas78@gmail.com>
2024-01-04 16:30:55 +0300

    modified my python docstring

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,33 +1,53 @@
 #!/usr/bin/python3
+"""Rectangle Module"""
+
 
 class Rectangle:
+    """Defines rectangle"""
 
     def __init__(self, height=0, width=0):
+        """
+        constructor
+
+        Args:
+            width: width of rectangle
+            height: height of rectangle
+        """
         self.width = width
         self.height = height
 
     @property
     def width(self):
+        """
+        Get or set width of rectangle
+        Returns:
+            ValueError: if width is less than 0
+            TypeError: if width not integer
+        """
         return self.__width
 
     @width.setter
     def width(self, value):
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
-        else:
-            self.__width = value
+        self.__width = value
 
     @property
     def height(self):
+        """
+        Gets or sets height of rectangle
+        Returns:
+            ValueError: if height is less than 0
+            TypeError: if height not integer
+        """
         return self.__height
 
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("height must be >=0")
-        else:
-            self.__height = value
+        self.__height = value
