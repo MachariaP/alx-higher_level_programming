600dc6a17cb9cfbc3927cf8283e3b2301b9605ad
MachariaP <walburphinehas78@gmail.com>
2024-01-09 14:58:16 +0300

    a python script  that reads a text file (UTF8) and prints it to stdout

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -9,5 +9,4 @@ def read_file(filename=" "):
     """
 
     with open(filename, 'r', encoding='utf-8') as file:
-        content = file.read()
-        print(content)
+        print(file.read(), end="")
