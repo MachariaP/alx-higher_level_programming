e5f8529f42eccbc3cba30ef3a55aef615e1b6676
MachariaP <walburphinehas78@gmail.com>
2024-01-14 15:20:02 +0300

    Update the class Rectangle by improving the public method def display(self)

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -94,8 +94,10 @@ class Rectangle(Base):
 
         """
 
+        for _ in range(self.__y):
+            print()
         for _ in range(self.__height):
-            print("#" * self.__width)
+            print(" " * self.__x + "#" * self.__width)
 
     def __str__(self):
         """
