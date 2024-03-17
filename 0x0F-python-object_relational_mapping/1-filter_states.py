705198acd1b08678f122b875a2de74981ba90aeb
MachariaP <walburphinehas78@gmail.com>
2024-03-17 18:53:26 +0300

    validated pycodestyle

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-This script lists all states with 
+This script lists all states with
 a `name` starting with the letter `N`
 from the database `hbtn_0e_0_usa`.
 """
@@ -18,15 +18,15 @@ from the database.
 if __name__ == '__main__':
     # Establishing a connection to the database
     db_connect = db.connect(host="localhost",
-            port=3306,
-            user=argv[1],
-            passwd=argv[2],
-            db=argv[3])
+                            port=3306,
+                            user=argv[1],
+                            passwd=argv[2],
+                            db=argv[3])
 
     # Creating a cursor object to execute SQL queries
     db_cursor = db_connect.cursor()
 
-    # Executing SQL query to select states starting with 'N' and ordering them by ID
+    # Executing SQL query to select states and ordering them
     db_cursor.execute(
             "SELECT * FROM states WHERE name LIKE BINARY 'N%' \
                     ORDER BY states.id ASC")
