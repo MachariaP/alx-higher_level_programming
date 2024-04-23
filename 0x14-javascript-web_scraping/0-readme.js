9489436da77460e2656c982c3e45d72323ca6713
MachariaP <walburphinehas78@gmail.com>
2024-04-23 20:38:02 +0300

    a script that reads and prints the content of a file

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const filename = process.argv[2];
+
+fs.readFile(filename, 'utf8', (err, data) => {
+	if (err) {
+		console.log(err);
+	} else {
+		console.log(data);
+	}
+});
