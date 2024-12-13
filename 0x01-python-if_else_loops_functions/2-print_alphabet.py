70d19ba90eaf302308f28eeeaa18da25b06e08b8
MachariaP <walburphinehas78@gmail.com>
2024-12-13 16:08:44 +0300

    feat: print lowercase ASCII alphabet without newline

---
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+# A program that prints the ASCII alphabet, in lowercase,
+# not followed by a new line.
+
+# Iterate over the characters in the ASCII alphabet
+for i in range(97, 123):
+    print(chr(i), end="")
